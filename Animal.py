553845ae2eb508934150655e4a2c034ae06cf3ee
Tahsin Ayon <tahsinayonn.com>
2025-02-21 20:56:58 -0500

    updated

--- Animal.py
+++ Animal.py
@@ -1,8 +1,26 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
-        print("hello, I am", self.__name)
+    def __init__(self, name, eating_habit, sleep_pattern, movement, aggression, water_activity):
+        self.name = name
+        self.eating_habit = eating_habit
+        self.sleep_pattern = sleep_pattern
+        self.movement = movement
+        self.aggression = aggression
+        self.__water_activity = water_activity  # Private attribute
 
-    def talk(self):
-        print("hi")
+    def make_sound(self):
+        print(f"{self.name} makes a unique sound.")
 
+    def eat(self):
+        print(f"{self.name} is enjoying its {self.eating_habit} meal.")
+
+    def sleep(self):
+        print(f"{self.name} sleeps {self.sleep_pattern}.")
+
+    def move(self):
+        print(f"{self.name} moves by {self.movement}.")
+
+    def attack(self):
+        print(f"{self.name} shows aggression by {self.aggression}.")
+
+    def swim(self):
+        print(f"{self.name} is engaging in {self.__water_activity}.")
